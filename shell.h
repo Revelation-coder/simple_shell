022a4eb2ec4667171421311592ef16c16eacbdad
Revelation-coder <82636655+Revelation-coder@users.noreply.github.com>
2023-02-20 02:48:06 -0800

    Update shell.h

--- shell.h
+++ shell.h
@@ -42,29 +42,6 @@ char *_strpbrk(char *s, char *accept);
 char *_strtok(char *s, const char *delim);
 unsigned int _strlen(char *s);
 
-/*linkedlist.c  */
-/**
- * struct list_s - singly linked list
- * @struct list_s: - singly linked list
- * @key: void * type p to the key
- * @value: void * type p to the value
- * @next: struct list_s ptr to the next node
- *
- * Description: sing linked list used in shell project
- */
-typedef struct list_s
-{
-void *key;
-void *value;
-struct list_s *next;
-} list_t;
-
-list_t *add_node(list_t **head, void *key, void *value);
-list_t *reverse_list(list_t **head);
-void free_list(list_t **head, int flag);
-int delete_node_at_index(list_t **head, unsigned int index);
-
-
 /* log.c */
 int add_log(sev_t *sev);
 int write_log(sev_t *sev);
